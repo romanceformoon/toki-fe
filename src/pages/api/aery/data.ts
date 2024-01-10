73d4a61eb6d0c760913b379afde598b9fd9f0dea
Jungwoo Lee <lmwljw98@gmail.com>
2024-01-11 04:10:53 +0900

    Feature/download link (#19)
    * style: main font changed
    
    * feat: 5keys aery download button added

--- src/pages/api/aery/data.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import data from "public/table/aery/data.json";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json(data);
-}
