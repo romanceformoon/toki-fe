5c81dd9b6ea27002ca7abcd85d2a67af1b2bc3cd
romanceformoon <lmwljw98@gmail.com>
2024-01-12 02:32:37 +0900

    style: dan color changed

--- src/components/UserNickname/index.tsx
+++ src/components/UserNickname/index.tsx
@@ -1,5 +1,6 @@
 import { Tooltip, Typography } from "@mui/material";
 import { ReactNode } from "react";
+import { aerySkillSimulators } from "~/const/skillSimulator";
 import { ClickableText } from "../ClickableText";
 
 interface IUserNicknameProps {
@@ -21,7 +22,7 @@ export const UserNickname = ({
             fontSize="24px"
             fontWeight={700}
             sx={{
-              color: "#D3CCE3",
+              color: aerySkillSimulators[clearDan].color,
               whiteSpace: "nowrap",
             }}
           >
@@ -39,7 +40,7 @@ export const UserNickname = ({
             fontSize="24px"
             fontWeight={700}
             sx={{
-              color: "#E32636",
+              color: aerySkillSimulators[clearDan].color,
               whiteSpace: "nowrap",
             }}
           >
@@ -57,7 +58,7 @@ export const UserNickname = ({
             fontSize="24px"
             fontWeight={700}
             sx={{
-              color: "#A9A9A9",
+              color: aerySkillSimulators[clearDan].color,
               whiteSpace: "nowrap",
             }}
           >
@@ -75,7 +76,7 @@ export const UserNickname = ({
             fontSize="24px"
             fontWeight={700}
             sx={{
-              color: "#3D9140",
+              color: aerySkillSimulators[clearDan].color,
               whiteSpace: "nowrap",
             }}
           >
@@ -93,7 +94,7 @@ export const UserNickname = ({
             fontSize="24px"
             fontWeight={700}
             sx={{
-              color: "#FFE135",
+              color: aerySkillSimulators[clearDan].color,
               whiteSpace: "nowrap",
             }}
           >
@@ -110,7 +111,7 @@ export const UserNickname = ({
           <Typography
             sx={{
               color: "#fff",
-              textShadow: `#FF8C00 0px 0px 5px, #FF8C00 0px 0px 10px, #FF8C00 0px 0px 15px, #FF8C00 0px 0px 20px, #FF8C00 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
+              textShadow: `${aerySkillSimulators[clearDan].color}, 0px 0px 5px, ${aerySkillSimulators[clearDan].color} 0px 0px 10px, ${aerySkillSimulators[clearDan].color} 0px 0px 15px, ${aerySkillSimulators[clearDan].color} 0px 0px 20px, ${aerySkillSimulators[clearDan].color} 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
               whiteSpace: "nowrap",
             }}
             fontSize="24px"
@@ -129,7 +130,7 @@ export const UserNickname = ({
           <Typography
             sx={{
               color: "#fff",
-              textShadow: `#FF0800 0px 0px 5px, #FF0800 0px 0px 10px, #FF0800 0px 0px 15px, #FF0800 0px 0px 20px, #FF0800 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
+              textShadow: `${aerySkillSimulators[clearDan].color} 0px 0px 5px, ${aerySkillSimulators[clearDan].color} 0px 0px 10px, ${aerySkillSimulators[clearDan].color} 0px 0px 15px, ${aerySkillSimulators[clearDan].color} 0px 0px 20px, ${aerySkillSimulators[clearDan].color} 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
               whiteSpace: "nowrap",
             }}
             fontSize="24px"
@@ -148,7 +149,7 @@ export const UserNickname = ({
           <Typography
             sx={{
               color: "#fff",
-              textShadow: `#7e748a6e 0px 0px 5px, #7e748a6e 0px 0px 10px, #7e748a6e 0px 0px 15px, #7e748a6e 0px 0px 20px, #7e748a6e 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
+              textShadow: `${aerySkillSimulators[clearDan].color} 0px 0px 5px, ${aerySkillSimulators[clearDan].color} 0px 0px 10px, ${aerySkillSimulators[clearDan].color} 0px 0px 15px, ${aerySkillSimulators[clearDan].color} 0px 0px 20px, ${aerySkillSimulators[clearDan].color} 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
               whiteSpace: "nowrap",
             }}
             fontSize="24px"
@@ -167,7 +168,7 @@ export const UserNickname = ({
           <Typography
             sx={{
               color: "#fff",
-              textShadow: `#5DADEC 0px 0px 5px, #5DADEC 0px 0px 10px, #5DADEC 0px 0px 15px, #5DADEC 0px 0px 20px, #5DADEC 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
+              textShadow: `${aerySkillSimulators[clearDan].color} 0px 0px 5px, ${aerySkillSimulators[clearDan].color} 0px 0px 10px, ${aerySkillSimulators[clearDan].color} 0px 0px 15px, ${aerySkillSimulators[clearDan].color} 0px 0px 20px, ${aerySkillSimulators[clearDan].color} 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
               whiteSpace: "nowrap",
             }}
             fontSize="24px"
@@ -186,7 +187,7 @@ export const UserNickname = ({
           <Typography
             sx={{
               color: "#fff",
-              textShadow: `#A020F0 0px 0px 5px, #A020F0 0px 0px 10px, #A020F0 0px 0px 15px, #A020F0 0px 0px 20px, #A020F0 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
+              textShadow: `${aerySkillSimulators[clearDan].color} 0px 0px 5px, ${aerySkillSimulators[clearDan].color} 0px 0px 10px, ${aerySkillSimulators[clearDan].color} 0px 0px 15px, ${aerySkillSimulators[clearDan].color} 0px 0px 20px, ${aerySkillSimulators[clearDan].color} 0px 0px 30px, 2px 2px 2px rgba(206,89,55,0)`,
               whiteSpace: "nowrap",
             }}
             fontSize="24px"
@@ -206,10 +207,10 @@ export const UserNickname = ({
             sx={{
               "@keyframes kaiden": {
                 from: {
-                  textShadow: `0 0 2px #fff, 0 0 4px #fff, 0 0 6px #FFD700, 0 0 8px #FFD700, 0 0 9px #FFD700, 0 0 10px #FFD700, 0 0 20px #FFD700`,
+                  textShadow: `0 0 2px #fff, 0 0 4px #fff, 0 0 6px ${aerySkillSimulators[clearDan].color}, 0 0 8px ${aerySkillSimulators[clearDan].color}, 0 0 9px ${aerySkillSimulators[clearDan].color}, 0 0 10px ${aerySkillSimulators[clearDan].color}, 0 0 20px ${aerySkillSimulators[clearDan].color}`,
                 },
                 to: {
-                  textShadow: `0 0 10px #fff, 0 0 15px #FFD700, 0 0 20px #FFD700, 0 0 25px #FFD700, 0 0 30px #FFD700, 0 0 35px #FFD700, 0 0 40px #FFD700`,
+                  textShadow: `0 0 10px #fff, 0 0 15px ${aerySkillSimulators[clearDan].color}, 0 0 20px ${aerySkillSimulators[clearDan].color}, 0 0 25px ${aerySkillSimulators[clearDan].color}, 0 0 30px ${aerySkillSimulators[clearDan].color}, 0 0 35px ${aerySkillSimulators[clearDan].color}, 0 0 40px ${aerySkillSimulators[clearDan].color}`,
                 },
               },
               animation: "kaiden 1s ease-in-out infinite alternate",
@@ -229,36 +230,36 @@ export const UserNickname = ({
     );
   }
 
-   if (clearDan === "GORILLA DAN") {
-     return (
-       <ClickableText onClick={onClick}>
-         <Tooltip title={clearDan}>
-           <Typography
-             sx={{
-               "@keyframes gorilla": {
-                 from: {
-                   textShadow: `0 0 2px #fff, 0 0 4px #fff, 0 0 6px #9B2335, 0 0 8px #9B2335, 0 0 9px #9B2335, 0 0 10px #9B2335, 0 0 20px #9B2335`,
-                 },
-                 to: {
-                   textShadow: `0 0 10px #fff, 0 0 15px #9B2335, 0 0 20px #9B2335, 0 0 25px #9B2335, 0 0 30px #9B2335, 0 0 35px #9B2335, 0 0 40px #9B2335`,
-                 },
-               },
-               animation: "gorilla 1s ease-in-out infinite alternate",
-               webkitAnimation: "gorilla 1s ease-in-out infinite alternate",
-               MozAnimation: "gorilla 1s ease-in-out infinite alternate",
+  if (clearDan === "GORILLA DAN") {
+    return (
+      <ClickableText onClick={onClick}>
+        <Tooltip title={clearDan}>
+          <Typography
+            sx={{
+              "@keyframes gorilla": {
+                from: {
+                  textShadow: `0 0 2px #fff, 0 0 4px #fff, 0 0 6px ${aerySkillSimulators[clearDan].color}, 0 0 8px ${aerySkillSimulators[clearDan].color}, 0 0 9px ${aerySkillSimulators[clearDan].color}, 0 0 10px ${aerySkillSimulators[clearDan].color}, 0 0 20px ${aerySkillSimulators[clearDan].color}`,
+                },
+                to: {
+                  textShadow: `0 0 10px #fff, 0 0 15px ${aerySkillSimulators[clearDan].color}, 0 0 20px ${aerySkillSimulators[clearDan].color}, 0 0 25px ${aerySkillSimulators[clearDan].color}, 0 0 30px ${aerySkillSimulators[clearDan].color}, 0 0 35px ${aerySkillSimulators[clearDan].color}, 0 0 40px ${aerySkillSimulators[clearDan].color}`,
+                },
+              },
+              animation: "gorilla 1s ease-in-out infinite alternate",
+              webkitAnimation: "gorilla 1s ease-in-out infinite alternate",
+              MozAnimation: "gorilla 1s ease-in-out infinite alternate",
 
-               color: "#fff",
-               whiteSpace: "nowrap",
-             }}
-             fontSize="24px"
-             fontWeight={700}
-           >
-             {children}
-           </Typography>
-         </Tooltip>
-       </ClickableText>
-     );
-   }
+              color: "#fff",
+              whiteSpace: "nowrap",
+            }}
+            fontSize="24px"
+            fontWeight={700}
+          >
+            {children}
+          </Typography>
+        </Tooltip>
+      </ClickableText>
+    );
+  }
 
   if (clearDan === "OVERJOY DAN") {
     return (
@@ -268,10 +269,10 @@ export const UserNickname = ({
             sx={{
               "@keyframes overjoy": {
                 from: {
-                  textShadow: `0 0 5px #fff, 0 0 8px #fff, 0 0 11px #833278, 0 0 14px #833278, 0 0 17px #833278, 0 0 20px #833278, 0 0 23px #833278`,
+                  textShadow: `0 0 5px #fff, 0 0 8px #fff, 0 0 11px ${aerySkillSimulators[clearDan].color}, 0 0 14px ${aerySkillSimulators[clearDan].color}, 0 0 17px ${aerySkillSimulators[clearDan].color}, 0 0 20px ${aerySkillSimulators[clearDan].color}, 0 0 23px ${aerySkillSimulators[clearDan].color}`,
                 },
                 to: {
-                  textShadow: `0 0 10px #fff, 0 0 15px #833278, 0 0 20px #833278, 0 0 25px #833278, 0 0 30px #833278, 0 0 35px #833278, 0 0 40px #833278`,
+                  textShadow: `0 0 10px #fff, 0 0 15px ${aerySkillSimulators[clearDan].color}, 0 0 20px ${aerySkillSimulators[clearDan].color}, 0 0 25px ${aerySkillSimulators[clearDan].color}, 0 0 30px ${aerySkillSimulators[clearDan].color}, 0 0 35px ${aerySkillSimulators[clearDan].color}, 0 0 40px ${aerySkillSimulators[clearDan].color}`,
                 },
               },
               animation: "overjoy 1s ease-in-out infinite alternate",
